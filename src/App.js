3eb60ab8c4b2da7a4762e7a6ccc13a01999f0b98
WhyHarvi <harvicustom@gmail.com>
2025-05-17 20:14:59 -0400

    Setup routing and folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import HomePage from './components/Home/HomePage';
+import ProductPage from './components/Product/ProductPage';
+import CartPage from './components/cart/CartPage';
+
 function App() {
   return (
-    <div>
-      <h1>Welcome to the eCommerce App</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/products" element={<ProductPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+    </Router>
   );
 }
 
